185a24da3c3dd8d9bcb52928838d733921e219ff
Jonathan Heroux <j.heroux@live.ca>
2025-07-24 12:32:39 -0400

    Ajouts d'animations, conditions protection du jeu et éléments finaux du projet. Version semi-final

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
 
 let secretWord = "";
 let language = "fr";
-let gameStarted = false;
 let startBtn = document.querySelector("#startGame");
 let resetBtn = document.querySelector("#restartGame");
-// let gameStarted = localStorage.getItem("gameStarted") === "true";
+let rightHandHero = document.querySelector(".rightHandHero");
+let knife = document.querySelector(".knife");
+let rope = document.querySelector(".rope");
+let barrel = document.querySelector(".barrel");
+let hanged = document.querySelector(".hanged");
+let revealedLetters = [];
+let guessedLetter=[];
+let chanceLeft = 7;
+let goodAnswerNumbers=0;
+let errorNumbers = 0;
+let gameOver = false;
+let animationInProgress = false;
+
+knife.addEventListener("animationend", (e) => {
+  if (e.animationName === "knifeToBarrel") {
+    onWrongAnswer();
+  }else if(e.animationName === "knifeToRope"){
+        onGoodAnswer();
+    }
+
+});
+
+
+
+const triedWords =[];
 const urlFr = "./data/mots_francais.json"
+const urlEn = "./data/mots_anglais.txt"
+const input = document.querySelector("#boxText");
 
 async function getFrenchWords(){
     const request = await fetch(urlFr,{
@@ -21,22 +46,22 @@ async function getFrenchWords(){
         for (let i=0; i < getWords.length; i++){
             const filtered = getWords[i];
 
-            if(filtered.length>=4 && filtered.length<=15 && /^[a-zA-ZéèîïëàâäéèêëîïôöùûüçÀÂÄÉÈÊËÎÏÔÖÙÛÜÇ]+$/.test(filtered)){
+            if(filtered.length>=4 && filtered.length<=12 && /^[a-zA-ZéèîïëàâäéèêëîïôöùûüçÀÂÄÉÈÊËÎÏÔÖÙÛÜÇ]+$/.test(filtered)){
                 wordsList.push(filtered);
             }
         }
         const randomWord = wordsList[Math.floor(Math.random()* wordsList.length)];
         secretWord = randomWord;
-        let hiddenWord = "";
+        revealedLetters = [];
             for ( let i=0; i < randomWord.length; i++){
-                hiddenWord += "_ ";
+                revealedLetters.push("_");
         }
-        document.getElementById("hideWord").textContent = hiddenWord; //hide the word
-        // document.getElementById("hideWord").textContent = secretWord; //Show the word
+        document.getElementById("hideWord").textContent = revealedLetters.join(" "); //hide the word for admin tools
+        // document.getElementById("hideWord").textContent = secretWord; //Show the word for admin tools
+        document.getElementById("winLose").textContent = "Bonne chance!";
+        resetElements();
     }
 }
-
-const urlEn = "./data/mots_anglais.txt"
 
 async function getEnglishWords(){
     console.log("getEnglishWords() appelé !")
@@ -54,45 +79,55 @@ async function getEnglishWords(){
         for (let i=0; i < getWords.length; i++){
             const filtered = getWords[i];
 
-            if(filtered.length>=5 && filtered.length<=10 && /^[a-zA-ZéèîïëàâäéèêëîïôöùûüçÀÂÄÉÈÊËÎÏÔÖÙÛÜÇ]+$/.test(filtered)){
+            if(filtered.length>=4 && filtered.length<=12 && /^[a-zA-ZéèîïëàâäéèêëîïôöùûüçÀÂÄÉÈÊËÎÏÔÖÙÛÜÇ]+$/.test(filtered)){
                 wordsList.push(filtered);
             }
         }
         
         const randomWord = wordsList[Math.floor(Math.random()* wordsList.length)];
         secretWord = randomWord;
-        let hiddenWord = "";
+        revealedLetters = [];
             for ( let i=0; i < randomWord.length; i++){
-                hiddenWord += "_ ";
+                revealedLetters.push("_");
         }
-        // document.getElementById("hideWord").textContent = hiddenWord; // hide the word
-        document.getElementById("hideWord").textContent = secretWord; //show the word
+        document.getElementById("hideWord").textContent = revealedLetters.join(" "); // hide the word for admin
+        // document.getElementById("hideWord").textContent = secretWord; //show the word for admin
+        document.getElementById("winLose").textContent = "Good luck!";
+        resetElements();
     }
 }
 
-document.getElementById("frButton").addEventListener("click", getFrenchWords);
-document.getElementById("enButton").addEventListener("click", getEnglishWords);
+function resetElements(){
+    rope.classList.remove("breakedRope");
+    rope.style.top = "21.5%";
+    barrel.style.left = "54%";
+    animationInProgress=false;
+    goodAnswerNumbers=0;
+    errorNumbers=0;
+    chanceLeft=7;
+    gameOver=false;
+    guessedLetter=[];
+    triedWords.length = 0;
+    input.style.borderColor="";
+    enableAllLetters();
+    
+}
+
 document.getElementById("startGame").addEventListener("click", ()=>{
-    gameStarted = true;
     document.getElementById("restartGame").style.display="inline-block";
     document.getElementById("startGame").style.display="none";
-    console.log("Texte du bouton start:", language);
     
     if(language === "en"){
         getEnglishWords();
-        
     }else{
         getFrenchWords();
-        
     }
 });
-document.getElementById("restartGame").addEventListener("click", ()=>{
-        
+document.getElementById("restartGame").addEventListener("click", async ()=>{
     if(language === "en"){
-        getEnglishWords();
-        
+        await getEnglishWords();
     }else{
-        getFrenchWords();
+        await getFrenchWords();
         
     }
 });
@@ -100,18 +135,123 @@ document.getElementById("restartGame").addEventListener("click", ()=>{
 const letterBox = document.querySelector("#boxLetter");
 const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+function disableAllLetters() {
+    document.querySelectorAll(".letterBtn").forEach(btn => btn.disabled = true);
+}
+function enableAllLetters() {
+    document.querySelectorAll(".letterBtn").forEach(btn => btn.disabled = false);
+}
+
+function revealSecretWord(){
+    for (let i=0; i< secretWord.length; i++){
+        revealedLetters[i] = secretWord[i];
+    }
+    document.getElementById("hideWord").textContent = revealedLetters.join(" ");
+}
+
+function getClick(clickedLetter,button){
+    if(gameOver || guessedLetter.includes(clickedLetter) || chanceLeft <=0)return;
+    if (animationInProgress)return;
+    animationInProgress=true;
+    disableAllLetters();
+
+    guessedLetter.push(clickedLetter);
+    button.disabled = true;
+    
+    let found = false;
+    for (let i=0; i<secretWord.length; i++){
+        if(secretWord[i].toUpperCase() === clickedLetter){
+            revealedLetters[i] = secretWord[i];
+            found = true;
+        }
+    }
+    document.getElementById("hideWord").textContent = revealedLetters.join(" ")
+
+    if(found){
+            playThrowAnimation(rope);
+        }else{
+            chanceLeft--;
+                if(chanceLeft<=0){
+                    chanceLeft=0;
+                    document.getElementById("winLose").textContent = language === "fr" ? "Le pendu est ... peut être sauverez-vous le prochain!" : "The hangman is ... maybe you will save the next one!";
+                    gameOver=true;
+                    revealSecretWord();
+                    playThrowAnimation(barrel)
+                    disableAllLetters();
+                }else{
+                    document.getElementById("winLose").textContent = language ==="fr" ? "Ce n'est pas terminé, il vous reste "+chanceLeft+" chance(s)" : "This is not over, you have "+chanceLeft+" chance(s) left!";
+                    playThrowAnimation(barrel); 
+        }
+    }
+}
+
 for(let i=0; i <alphabet.length; i++){
     const keysButton = document.createElement("button");
     keysButton.textContent = alphabet[i];
     keysButton.classList.add("letterBtn");
+    
+
+    keysButton.addEventListener("click", ()=>{
+        getClick(alphabet[i],keysButton);
+    });
+
     letterBox.appendChild(keysButton);
 }
+disableAllLetters();
 
-let rightHandHero = document.querySelector(".rightHandHero");
-let knife = document.querySelector(".knife");
-let rope = document.querySelector(".rope");
-let barrel = document.querySelector(".barrel");
-let hanged = document.querySelector(".hanged");
+document.querySelector("#guessWord").addEventListener("submit", (e)=>{
+    e.preventDefault();
+    
+    if(animationInProgress || gameOver || chanceLeft <=0)return;
+    
+    animationInProgress=true;
+    disableAllLetters();
+
+    const input = document.querySelector("#boxText");
+    const guess = input.value.trim();
+
+    input.value="";
+    input.focus();
+    input.style.borderColor="";
+
+    if(!/^[a-zA-ZéèîïëàâäéèêëîïôöùûüçÀÂÄÉÈÊËÎÏÔÖÙÛÜÇ]+$/.test(guess) || guess.length<4){
+        input.style.borderColor="darkred";
+        return;
+    }
+
+    if(triedWords.includes(guess)){
+        document.getElementById("winLose").textContent=language === "fr" ? "Oups, vous avez déjà essayé ce mot!" : "Oupsy, you've already tried this word!";
+        return;
+    }else{
+        triedWords.push(guess);
+    }
+
+    if(guess === secretWord){
+         for (let i=0; i<secretWord.length; i++){
+            revealedLetters[i] = secretWord[i];
+    }
+        
+        document.getElementById("hideWord").textContent = revealedLetters.join(" ")
+        playThrowAnimation(rope);
+    }else{
+        if(chanceLeft >0)
+        chanceLeft--;
+        document.getElementById("winLose").textContent = language ==="fr" ? "Ce n'est pas terminé, il vous reste "+chanceLeft+" chance(s)" : "This is not over, you have "+chanceLeft+" chance(s) left!";
+        onWrongAnswer();
+        playThrowAnimation(barrel);
+        
+        if(chanceLeft <=0){
+            document.getElementById("winLose").textContent = language === "fr" ? "Le pendu est ... peut être sauverez-vous le prochain!" : "The hangman is ... maybe you will save the next one!";
+            gameOver=true;
+            revealSecretWord();
+            chanceLeft=0;
+            onWrongAnswer();
+            playThrowAnimation(barrel);
+            disableAllLetters();
+        }
+    }
+      
+});
 
 let translation = {
     fr: {
@@ -122,7 +262,8 @@ let translation = {
         frButton: "Français",
         enButton: "Anglais",
         start: "Commencez le jeu",
-        restart:"Recommencer"
+        restart:"Recommencer",
+        submitButton:"Valider"
 },
     en: {
         header: `Welcome to <br>"Will you save the hangman?"`,
@@ -132,7 +273,8 @@ let translation = {
         frButton: "French",
         enButton: "English",
         start: "Start the game",
-        restart: "Restart"
+        restart: "Restart",
+        submitButton: "Submit"
         }
 };
 
@@ -144,6 +286,8 @@ function switchLanguage(language){
     document.querySelector("#boxText").placeholder = translation[language].boxText;
     document.querySelector("#startGame").textContent = translation[language].start;
     document.querySelector("#restartGame").textContent = translation[language].restart;
+    document.querySelector(".submitButton").textContent = translation[language].submitButton;
+    document.querySelector("#hideWord").textContent = translation[language].hideWord;
 }
 
 
@@ -153,20 +297,71 @@ document.querySelector("#frButton").addEventListener("click", ()=>{
     language = "fr";
     document.getElementById("restartGame").style.display="none";
     document.getElementById("startGame").style.display="inline-block";
+    document.getElementById("hideWord").textContent = translation.fr.hideWord;
 });
 document.querySelector("#enButton").addEventListener("click", ()=>{
     switchLanguage("en")
     language = "en";
     document.getElementById("restartGame").style.display="none";
     document.getElementById("startGame").style.display="inline-block";
+    document.getElementById("hideWord").textContent = translation.en.hideWord;
 });
 
+function playThrowAnimation(target){
+    const hand = document.getElementById("throwHand");
+    const animatedKnife = document.getElementById("animatedKnife");
+
+    hand.classList.remove("throw-hand");
+    animatedKnife.classList.remove("throwToRope");
+    animatedKnife.classList.remove("throwToBarrel");
+
+    void hand.offsetWidth;
+    void animatedKnife.offsetWidth;
+
+    hand.classList.add("throw-hand");
+    if(target.classList.contains("rope")){
+    animatedKnife.classList.add("throwToRope");
+    }else if(target.classList.contains("barrel")){
+    animatedKnife.classList.add("throwToBarrel");
+    }
+
+    setTimeout(() => {
+        animatedKnife.style.opacity="1";
+        animatedKnife.style.transform="none";
+        animatedKnife.classList.remove("throwToRope","throwToBarrel");
+        animationInProgress=false;
+        if(!gameOver) enableAllLetters();
+    }, 1700);
+
+};
 
 
-function heroAnimation(){
-    rightHandHero.style.transition = "transform 0.3s ease";
-    rightHandHero.style.transform = "rotate(-45deg)";
-    setTimeout(() =>{
-        rightHandHero.style.transform = "rotate(0deg)";
-}, 300);
+function onGoodAnswer(){
+    goodAnswerNumbers++;
+    let baseTopPosition = 21.5;
+    let ropeDown = 0.3;
+    let newTopPosition = baseTopPosition + goodAnswerNumbers * ropeDown;
+    rope.style.top = newTopPosition+"%";
+
+    if (!revealedLetters.includes("_")) {
+        revealSecretWord();
+        document.getElementById("winLose").textContent = language === "fr" ? "Félicitation, vous avez sauvé le pendu!" : "Congratulations, you've saved the hangman!";
+        gameOver = true;
+        disableAllLetters();
+        animatedRopeBreak();
+    }
+}
+
+function onWrongAnswer(){
+    errorNumbers++;
+    let baseLeftPosition = 54;
+    let barrelLeft = -0.63;
+    let newLeftPosition= baseLeftPosition + errorNumbers * barrelLeft;
+    barrel.style.left = newLeftPosition + "%";
+}
+
+function animatedRopeBreak(){
+    let currentTop = parseFloat(getComputedStyle(rope).top);
+    rope.style.top = currentTop + "%";
+    rope.classList.add("breakedRope");
 }
